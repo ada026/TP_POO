68e31240e42f951d699c013e9615affc6a7dcf76
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2017-12-14 18:20:00 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args){
         User user1 = new User("elyes");
-
+        System.out.println("Test pull 1 ");
+        System.out.println("Test pull 1  2");
+        
     }
 }
